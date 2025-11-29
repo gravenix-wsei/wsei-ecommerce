fa2b917b8e55f9f6ac4aedac02076acc990cf791
Bartłomiej Talarczyk <btalarczyk1@gmail.com>
2025-11-29 20:41:31 +0100

    fix username and password

--- ecommerce/migrations/Version20251129182044.php
+++ ecommerce/migrations/Version20251129182044.php
@@ -22,14 +22,14 @@ final class Version20251129182044 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
 
-        // Insert admin user with hashed password (password: admin123)
-        $this->addSql("INSERT INTO user (email, roles, password) VALUES ('admin@example.com', '[\"ROLE_ADMIN\"]', '\$2y\$13\$Fjuep1FKuSnPByKJ7zMaEO/RdMU726zUmPzvRElqXa5xzQ3em95Wi')");
+        // Insert admin user with hashed password (username: admin, password: password)
+        $this->addSql("INSERT INTO user (email, roles, password) VALUES ('admin', '[\"ROLE_ADMIN\"]', '\$2y\$13\$1jNuwZbjOIK3O5A330Qy2.NfVPVa5VBDzZfZxQHudy5mpY3XC4OS2')");
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql("DELETE FROM user WHERE email = 'admin@example.com'");
+        $this->addSql("DELETE FROM user WHERE email = 'admin'");
         $this->addSql('DROP TABLE user');
     }
 }
