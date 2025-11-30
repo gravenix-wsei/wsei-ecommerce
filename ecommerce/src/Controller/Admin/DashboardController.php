de0f32d4b6f3ec342a67bd54f80349eb3ae5440b
Bartłomiej Talarczyk <btalarczyk1@gmail.com>
2025-11-30 11:03:10 +0100

    wsei-ecommerce-2 feat: Dashboard administratora i konfiguracja bezpieczeństwa

--- ecommerce/src/Controller/Admin/DashboardController.php
+++ ecommerce/src/Controller/Admin/DashboardController.php
@@ -14,6 +14,6 @@ class DashboardController extends AbstractController
     #[Route('/dashboard', name: 'admin.dashboard')]
     public function index(): Response
     {
-        return $this->render('admin/dashboard.html.twig');
+        return $this->render('admin/pages/dashboard.html.twig');
     }
 }
