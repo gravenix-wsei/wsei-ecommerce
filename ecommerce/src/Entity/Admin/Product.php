8cfda66704a6ace9833b119b495bff6a3003ae11
Bartłomiej Talarczyk <41484847+gravenix@users.noreply.github.com>
2025-11-30 19:49:07 +0100

    feat: CRUD Produktów (MVP) (#15)
    * wsei-ecommerce-5 feat: CRUD Produktów (MVP)
    
    * wsei-ecommerce-5 feat: CRUD Produktów (MVP)

---
+++ ecommerce/src/Entity/Admin/Product.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wsei\Ecommerce\Entity\Admin;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Wsei\Ecommerce\Repository\Admin\ProductRepository;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[ORM\Table(name: 'product')]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: 'integer')]
+    private ?int $stock = null;
+
+    #[ORM\Column(type: 'decimal', precision: 10, scale: 2)]
+    private ?string $priceNet = null;
+
+    #[ORM\Column(type: 'decimal', precision: 10, scale: 2)]
+    private ?string $priceGross = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getPriceNet(): ?string
+    {
+        return $this->priceNet;
+    }
+
+    public function setPriceNet(string $priceNet): self
+    {
+        $this->priceNet = $priceNet;
+
+        return $this;
+    }
+
+    public function getPriceGross(): ?string
+    {
+        return $this->priceGross;
+    }
+
+    public function setPriceGross(string $priceGross): self
+    {
+        $this->priceGross = $priceGross;
+
+        return $this;
+    }
+}
