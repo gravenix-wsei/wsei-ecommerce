2d7798f74beeb612c5cca8cde478e828dde82dca
Bartłomiej Talarczyk <41484847+gravenix@users.noreply.github.com>
2025-12-03 23:45:44 +0100

    feat: Konfiguracja środowiska testowego (PHPUnit) (#46)
    * wsei-ecommerce-16 feat: Konfiguracja środowiska testowego (PHPUnit)
    
    * add composer.json scripts
    
    * docker compose env fix

--- ecommerce/config/bundles.php
+++ ecommerce/config/bundles.php
@@ -12,11 +12,15 @@ return [
     ],
     Symfony\Bundle\MakerBundle\MakerBundle::class => [
         'dev' => true,
+        'test' => true,
     ],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => [
         'all' => true,
     ],
     Symfony\Bundle\TwigBundle\TwigBundle::class => [
         'all' => true,
+    ],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => [
+        'test' => true,
     ],
 ];
---
+++ ecommerce/tests/UnitTest/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Wsei\Ecommerce\Tests\UnitTest;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
