0216d9d33f9d35786a4a6eb2f222e200633d61f0
Bartłomiej Talarczyk <btalarczyk1@gmail.com>
2025-12-11 21:42:17 +0100

    wsei-ecommerce-18 feat: Obsługa Koszyka Zakupowego (Cart & CartItem)

--- ecommerce/src/Repository/CartItemRepository.php
+++ ecommerce/src/Repository/CartItemRepository.php
@@ -32,4 +32,3 @@ class CartItemRepository extends ServiceEntityRepository
             ->getOneOrNullResult();
     }
 }
-
--- ecommerce/src/Repository/CartRepository.php
+++ ecommerce/src/Repository/CartRepository.php
@@ -33,4 +33,3 @@ class CartRepository extends ServiceEntityRepository
             ->getOneOrNullResult();
     }
 }
-
